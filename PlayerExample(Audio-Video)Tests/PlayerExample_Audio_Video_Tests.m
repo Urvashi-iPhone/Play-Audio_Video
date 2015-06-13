051656b52245cd4a76636c302afb226377a84555
MAC <mac@MACs-iMac.local>
2015-06-13 12:36:34 +0530

    Initial Commit

---
+++ PlayerExample(Audio-Video)Tests/PlayerExample_Audio_Video_Tests.m
@@ -0,0 +1,40 @@
+//
+//  PlayerExample_Audio_Video_Tests.m
+//  PlayerExample(Audio-Video)Tests
+//
+//  Created by MAC on 6/13/15.
+//  Copyright (c) 2015 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PlayerExample_Audio_Video_Tests : XCTestCase
+
+@end
+
+@implementation PlayerExample_Audio_Video_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
